2759a348f3c9a2c0c50db1aa7df2171f2e09e955
Waqar Ali <waqar.ali@wohlig.com>
2024-05-23 18:11:41 +0530

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,58 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
+import { CustomButton } from "../components";
+
+const Home = () => {
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET&apos;S <br className="xl:block hidden" /> DO IT
+              </h1>
+            </motion.div>
+            <motion.div
+              className="flex flex-col gap-5"
+              {...headContentAnimation}
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Create your own unique & exclusive t-shirt witth our brand new
+                customization tool. <strong>Unleash your imagination</strong> &
+                define your style
+              </p>
+
+              <CustomButton
+                type="filled"
+                title="Customize It"
+                handleClick={() => (state.intro = false)}
+                customStyles="w-fit px-4 py-2.5 font-bold text-small"
+              />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Home;
